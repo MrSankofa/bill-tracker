562de2ef428e3f095626ebe4d6739778cda58e2a
brett.cunningham <brett.cunningham@sbevision.com>
2024-11-18 21:01:22 -0500

    fix(testing): for app component

--- apps/bill-tracker/src/app/app.component.spec.ts
+++ apps/bill-tracker/src/app/app.component.spec.ts
@@ -15,7 +15,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome bill-tracker'
+      'Bill Tracker'
     );
   });
 
