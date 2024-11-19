ddc13bc77122cce46e5e9ac55314d104046592be
brett.cunningham <brett.cunningham@sbevision.com>
2024-11-18 19:54:35 -0500

    add(js/ts): more bill actions

--- apps/bill-tracker/src/app/+state/bills.actions.ts
+++ apps/bill-tracker/src/app/+state/bills.actions.ts
@@ -12,3 +12,7 @@ export const loadBillsFailure = createAction(
   '[Bills/API] Load Bills Failure',
   props<{ error: any }>()
 );
+
+export const addBill = createAction('[Bills] Add Bill', props<{ bill: BillsEntity }>());
+export const updateBill = createAction('[Bills] Update Bill', props<{ bill: BillsEntity }>());
+export const deleteBill = createAction('[Bills] Delete Bill', props<{ id: number }>());
